309fe9be43cf9fd6332a9c2caf2a585b89a821eb
mirage <priyagung.yoggi@gmail.com>
2024-05-24 14:24:37 +0700

    pemantaun 1

--- src/pages/dashboard/user.vue
+++ src/pages/dashboard/user.vue
@@ -44,28 +44,11 @@
 
 import NavbarLayout from '@/layouts/dashboard/navbarLayout.vue';
 import SidebarLayout from '@/layouts/dashboard/sidebarLayout.vue';
+import axios from 'axios'
 let jsonData = []
-// Function to generate a random public IP address
-const generatePublicIp = () => `203.0.113.${Math.floor(Math.random() * 256)}`;
-
-// Function to generate a unique user agent
-const generateUserAgent = () => {
-  const browsers = ['Chrome', 'Firefox', 'Safari', 'Edge', 'Opera'];
-  const randomBrowser = browsers[Math.floor(Math.random() * browsers.length)];
-  const version = Math.floor(Math.random() * 50) + 50; // Random version between 50 and 100
-  return `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ${randomBrowser}/${version}.0 Safari/537.3`;
-};
-
-// Generate 10 data entries
-for (let i = 1; i <= 10; i++) {
-  jsonData.push({
-    name: `student student ${i < 10 ? '0' : ''}${i}`,
-    first_ip: generatePublicIp(),
-    last_ip: generatePublicIp(),
-    last_access: `2024-01-31T16:${i < 10 ? '0' : ''}${i}:22:10Z`,
-    user_agent: generateUserAgent(),
-    status: 500
-  });
+const consumeUser = () => {
+    jsonData.push(axios.get('http://localhost:3000/users'))
+    return jsonData
 }
 export default {
     components: {NavbarLayout, SidebarLayout},
