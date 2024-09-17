9c00864082abe8fddb027b1ad4a4426e77e2404e
Priyagung Yogi <priyagung.yoggi@gmail.com>
2024-09-17 12:39:30 +0700

    Update export-sql.sh

--- backend/bash/export-sql.sh
+++ backend/bash/export-sql.sh
@@ -10,4 +10,4 @@ mkdir $fullpath
 mv /var/lib/mysql-files/*.csv $fullpath 
 echo $fullpath
 sleep 5
-sshpass -p admin@12345 rsync -a --remove-source-files -e 'ssh -p 224' /var/lib/mysql-files/$min linux@180.250.135.11:~/backup-sql
+sshpass -p rsync -a --remove-source-files -e 'ssh -p 224' /var/lib/mysql-files/$min linux@:~/backup-sql
