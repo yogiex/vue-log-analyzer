59e627fb9d9b418cae067324b25386c301505d3a
Priyagung Elza Yogitama <priyagung.yoggi@gmail.com>
2025-01-18 22:52:45 +0700

    Update logs.vue

--- src/pages/dashboard/logs.vue
+++ src/pages/dashboard/logs.vue
@@ -1,19 +1,37 @@
 <template>
     <v-layout class="rounded rounded-md" style="min-height: 100vh;">
-        <NavbarLayout/>
-        <SidebarLayout/>
+        <NavbarLayout />
+        <SidebarLayout />
         <v-main>
             <v-container>
                 <v-card>
+                    <template v-slot:text>
+                        <v-text-field v-model="search" label="Search" prepend-inner-icon="mdi-magnify" single-line
+                            variant="outlined" hide-details>
+                        </v-text-field>
+                    </template>
                     <v-row>
-                        <v-col><v-date-input class="mt-5 mx-5" label="Date input time start" v-model="startDate" variant="outlined"></v-date-input></v-col>
-                        <v-col><v-date-input class="mt-5 mx-5" label="Date input time end" v-model="endDate" variant="outlined"></v-date-input></v-col>
+                        <v-col><v-date-input class="mt-5 mx-5" label="Date input time start" v-model="startDate"
+                                variant="outlined"></v-date-input></v-col>
+                        <v-col><v-date-input class="mt-5 mx-5" label="Date input time end" v-model="endDate"
+                                variant="outlined"></v-date-input></v-col>
                     </v-row>
                     <v-row>
                         <v-col>
-                            <v-btn @click="fetchLogs" variant="tonal" class="ml-4 mb-5" color="primary">Get These Logs</v-btn>
+                            <v-btn @click="fetchLogs" variant="tonal" class="ml-4 mb-5" color="primary">Get These
+                                Logs</v-btn>
                         </v-col>
                     </v-row>
+                    <!-- Progress circular -->
+                    <!-- Progress Circular -->
+                    <div v-if="loading" class="text-center">
+                        <v-progress-circular indeterminate :size="70" :width="7" color="primary"></v-progress-circular>
+                    </div>
+                    <!-- end of progress circular -->
+                    <!--  data tables-->
+                    <v-data-table :headers="headers" :items="logs" :search="search" dense>
+
+                    </v-data-table>
                 </v-card>
                 <!-- Error Dialog -->
                 <v-dialog v-model="dialog" width="auto">
@@ -21,10 +39,11 @@
                         <v-card-title class="text-h6">Error</v-card-title>
                         <v-card-text>{{ errorMessage }}</v-card-text>
                         <v-card-actions>
-                        <v-btn class="ms-auto" text @click="dialog = false">Ok</v-btn>
+                            <v-btn class="ms-auto" text @click="dialog = false">Ok</v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
+                <!-- end of Dialog -->
             </v-container>
         </v-main>
     </v-layout>
@@ -39,31 +58,46 @@ import axios from 'axios';
 export default {
     components: { dashboardLayout, SidebarLayout, NavbarLayout },
     data() {
-    return {
-        startDate: null, 
-        endDate: null, 
-        dialog: false,
-        errorMessage: "",
-    };
-  },
-  methods: {
-    async fetchLogs() {
-        if (this.startDate && this.endDate) {
-                
+        return {
+            startDate: null,
+            endDate: null,
+            dialog: false,
+            errorMessage: "",
+            headers: [
+                { key: 'user_id', title: ' User ID' },
+                { key: 'action', title: ' Action' },
+                { key: 'component', title: ' Component' },
+                { key: 'course_name', title: ' Course Name' },
+                { key: 'ip', title: ' IP Address' },
+                { key: 'log_id', title: ' Log ID' },
+                { key: 'quiz_id', title: ' Quiz ID' },
+                { key: 'quiz_name', title: ' Quiz Name' },
+                { key: 'target', title: ' Target' },
+                { key: 'timecreated', title: ' Time Created' },
+                { key: 'user_firstname', title: 'User Firstname' }
+            ],
+            search: '',
+            logs: [],
+            loading: false
+        };
+    },
+    methods: {
+        async fetchLogs() {
+            if (this.startDate && this.endDate) {
+                this.loading = true
                 const startTime = Math.floor(new Date(this.startDate).getTime() / 1000);
                 const endTime = Math.floor(new Date(this.endDate).getTime() / 1000);
 
-                
                 const apiUrl = `${import.meta.env.VITE_URL_FLASK_API}/api/get/logs?start_time=${startTime}&end_time=${endTime}`;
-
                 try {
-                    
+
                     const response = await axios.get(apiUrl);
-                    const logs = response.data;
-                    console.log("Fetched logs:", logs);
-                    
-                    
-                    } catch (error) {
+                    this.logs = response.data;
+                    console.table(response.data)
+                    console.log("Fetched logs:", this.logs);
+
+
+                } catch (error) {
                     // Handle errors and show modal
                     console.error("Error fetching logs:", error);
                     this.dialog = true; // Show the dialog
@@ -74,6 +108,8 @@ export default {
                     } else {
                         this.errorMessage = error.message || "An error occurred.";
                     }
+                } finally {
+                    this.loading = false;
                 }
             } else {
                 console.warn("Please select both start and end dates.");
@@ -81,6 +117,6 @@ export default {
                 this.dialog = true; // Open the dialog
             }
         }
-  },
+    },
 }
 </script>
