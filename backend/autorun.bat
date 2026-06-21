5c2c9025f13a47b55a10cc4f9a3d13798279a55c
yogiex <priyagung.yoggi@gmail.com>
2026-06-21 11:53:39 +0700

    chore(backend): remove legacy files and old models

--- backend/autorun.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Get the directory where the batch file is located
-cd D:\TUGAS KULIAH\Project Tugas Akhir\TugasAkhir\
-
-REM Activate the virtual environment
-call venv\Scripts\activate
-
-REM Run the Flask application
-py "app testing.py"
